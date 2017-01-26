94dbac57856b53e73c5d5c87c30a24201114a192
Jakaria Mahmood <jakaria.mahmood@mail.rakuten.com>
2017-01-26 12:32:20 +0900

    adding cache annotations

--- SpringCacheDemo/src/main/java/hello/SpringCacheDemoApplication.java
+++ SpringCacheDemo/src/main/java/hello/SpringCacheDemoApplication.java
@@ -2,11 +2,13 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SpringCacheDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCacheDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCacheDemoApplication.class, args);
+    }
 }
